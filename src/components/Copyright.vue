6408f1e92a733832afc2c6e45afaf288c126a90a
aiplat <womendi@qq.com>
2020-03-01 14:12:46 +0800

    添加大写开头vue组件

---
+++ src/components/Copyright.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cm_pf cm_w100 cm_hl3 cm_tc" :class="placeC1" v-if="copyrightData&&copyrightData.copyright">
+    <span :class="c1">{{copyrightData.copyright}} {{copyrightData.name}} {{copyrightData.site}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  export default Vue.extend({
+    props: {
+      placeC1: {
+        type: String,
+        default: 'cm_bl0',
+      },
+      c1: {
+        type: String,
+        default: 'cm_c3c',
+      },
+      copyrightData: {
+        type: Object,
+        default() {
+          return {
+            copyright: '',
+            name: '',
+            site: '',
+            placeClass: '',
+          };
+        },
+      },
+    },
+  });
+</script>
